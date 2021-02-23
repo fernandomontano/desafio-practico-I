0df2a9e98631e941bd5aac264897cbb788662b3f
Fernando Montano <fernando.montano41@gmail.com>
2021-02-22 21:48:42 -0600

    Subir ejercicio 2

---
+++ C#/Ejercicio-2/Ejercicio-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Recolección de datos";
+            double numeroUno, numeroDos, numeroTres, numeroCuatro, suma, multiplicacion;
+
+            Console.WriteLine("Digite 4 numeros");
+
+            Console.Write("1. ");
+            numeroUno = double.Parse(Console.ReadLine());
+            Console.Write("2. ");
+            numeroDos = double.Parse(Console.ReadLine());
+            Console.Write("3. ");
+            numeroTres = double.Parse(Console.ReadLine());
+            Console.Write("4. ");
+            numeroCuatro = double.Parse(Console.ReadLine());
+
+            Console.Title = "Presentación de resultados";
+            suma = numeroUno + numeroDos;
+            multiplicacion = numeroTres * numeroCuatro;
+
+            Console.WriteLine($"La suma de los primeros dos números ({numeroUno} + {numeroDos}) es: {suma}. Y, el producto de los otros dos ({numeroTres} * {numeroCuatro}) es {multiplicacion}"); 
+            Console.ReadKey();
+        }
+    }
+}
